3726ceed3f543f9992beba4c742535821b72362d
AlexeyBarabash <alexey@brave.com>
2021-06-15 14:53:54 +0300

    Revert "Use BraveRenderViewContextMenu::Show() override"
    This reverts commit 8b7394bc5935be51aa4c20c4ee8a5889fe4ed796.

--- chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chromium_src/chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -327,8 +327,3 @@ void BraveRenderViewContextMenu::InitMenu() {
     menu_model_.RemoveItemAt(index);
 #endif
 }
-
-void BraveRenderViewContextMenu::Show() {
-  RemoveAdjacentSeparators();
-  RenderViewContextMenu_Chromium::Show();
-}
