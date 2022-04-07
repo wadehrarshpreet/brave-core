3b75f94dec1c19ab0512950efd8a5420656ef7ad
Nick von Pentz <nickvonpentz@protonmail.com>
2022-04-07 11:12:47 -0400

    Address feedback
    * Use kMethodNotSupported instead of kInvalidParams
    
    * Add testcase for invalid URI scheme
    
    * Add testcase for empty string as JSON in data URI metadata

--- components/brave_wallet/browser/json_rpc_service.cc
+++ components/brave_wallet/browser/json_rpc_service.cc
@@ -1514,8 +1514,8 @@ void JsonRpcService::OnGetSupportsInterfaceERC721Metadata(
 
   if (!is_supported) {
     std::move(callback).Run(
-        "", mojom::ProviderError::kInvalidParams,
-        l10n_util::GetStringUTF8(IDS_WALLET_INVALID_PARAMETERS));
+        "", mojom::ProviderError::kMethodNotSupported,
+        l10n_util::GetStringUTF8(IDS_WALLET_METHOD_NOT_SUPPORTED_ERROR));
     return;
   }
 
@@ -1567,8 +1567,8 @@ void JsonRpcService::OnGetERC721TokenUri(
   if (scheme != url::kDataScheme && scheme != url::kHttpsScheme &&
       scheme != ipfs::kIPFSScheme) {
     std::move(callback).Run(
-        "", mojom::ProviderError::kInvalidParams,
-        l10n_util::GetStringUTF8(IDS_WALLET_INVALID_PARAMETERS));
+        "", mojom::ProviderError::kMethodNotSupported,
+        l10n_util::GetStringUTF8(IDS_WALLET_METHOD_NOT_SUPPORTED_ERROR));
     return;
   }
 
